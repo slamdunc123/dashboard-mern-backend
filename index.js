1a267d6e50fbf0dbd9164b6f108c41fcd0fecc0b
duncanlaidlaw <slamdunc123@gmail.com>
2021-11-02 04:44:36 +0000

    add test api

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(cors());
 
+// test api
+app.get('/', (req, res) => {
+	res.send('Express Backend Up And Running');
+});
+
 let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
