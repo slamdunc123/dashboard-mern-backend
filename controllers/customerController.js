58841fcf69ee728290ece6669adaf213e1aa7c75
duncanlaidlaw <slamdunc123@gmail.com>
2022-03-28 22:15:20 +0100

    add delete customer functionality

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -3,11 +3,35 @@ const Customer = require('../models/Customer');
 const getAll = async (req, res) => {
 	try {
 		const customers = await Customer.find();
-        res.json(customers)
+		res.json(customers);
 	} catch (err) {
 		console.error(err.error);
 		res.status(500).send('Server error');
 	}
 };
 
-module.exports = { getAll };
+const deleteById = async (req, res) => {
+	console.log(req);
+	try {
+		// check if customer exists
+		const customer = await Customer.findById(req.params.id);
+
+		// if id is a valid format but doesn't exist in database
+		if (!customer) {
+			return res.status(404).json({
+				msg: 'Customer not found',
+			});
+		}
+
+		await customer.remove();
+
+		res.json({
+			msg: 'Customer deleted successfully',
+		});
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+};
+
+module.exports = { getAll, deleteById };
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -5,5 +5,6 @@ const customerController = require('../controllers/customerController');
 // get all customers - http://localhost:5000/api/customers
 
 router.get('/', customerController.getAll);
+router.delete('/:id', customerController.deleteById)
 
 module.exports = router;
