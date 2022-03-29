2dfc78c3bed2c435c74664a8cfcc934c53c28d7f
duncanlaidlaw <slamdunc123@gmail.com>
2022-03-29 14:36:11 +0100

    initial commit after cloning

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
-// get all customers - http://localhost:5000/api/customers
-
+// get all customers - eg http://localhost:5000/api/customers
 router.get('/', customerController.getAll);
+
+// delete customer by id - eg http://localhost:5000/api/tests/624307d6280daad241023611
 router.delete('/:id', customerController.deleteById)
 
 module.exports = router;
