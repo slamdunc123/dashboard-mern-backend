4f27610778389ecfe12a23089fef69bf01d6c50a
duncanlaidlaw <slamdunc123@gmail.com>
2022-03-29 22:22:07 +0100

    rename app, add api routes

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -10,13 +10,45 @@ const getAll = async (req, res) => {
 	}
 };
 
+const getById = async (req, res) => {
+	try {
+		const customers = await Customer.find({ _id: req.params.id });
+		res.json(customers);
+	} catch (err) {
+		console.error(err.error);
+		res.status(500).send('Server error');
+	}
+};
+
+const create = async (req, res) => {
+	try {
+		let customer = await Customer.findOne({
+			name: req.body.name,
+		});
+		if (customer) {
+			return res.status(400).json({
+				errors: [{ msg: 'Customer already exists' }],
+			});
+		}
+		const newCustomer = new Customer({
+			name: req.body.name,
+			email: req.body.email,
+			plan: req.body.plan,
+		});
+
+		customer = await newCustomer.save();
+		res.json({ customer: customer, msg: 'Customer created' });
+	} catch (err) {
+		console.error(err.error);
+		re.status(500).send('Server error');
+	}
+};
+
 const deleteById = async (req, res) => {
 	console.log(req);
 	try {
-		// check if customer exists
 		const customer = await Customer.findById(req.params.id);
 
-		// if id is a valid format but doesn't exist in database
 		if (!customer) {
 			return res.status(404).json({
 				msg: 'Customer not found',
@@ -34,4 +66,26 @@ const deleteById = async (req, res) => {
 	}
 };
 
-module.exports = { getAll, deleteById };
+const updateById = async (req, res) => {
+	try {
+		let customer = await Customer.findById(req.params.id);
+
+		if (!customer) {
+			return res.status(404).json({
+				msg: 'Customer not found',
+			});
+		}
+		customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		});
+		await res.json({
+			msg: 'Customer updated successfully.',
+			customer: customer,
+		});
+	} catch (err) {
+		console.error(err.error);
+		res.status(500).send('Server error');
+	}
+};
+
+module.exports = { getAll, getById, create, deleteById, updateById };
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
-// get all customers - eg http://localhost:5000/api/customers
+// get all customers - http://localhost:5000/api/customers
 router.get('/', customerController.getAll);
 
-// delete customer by id - eg http://localhost:5000/api/tests/624307d6280daad241023611
+// get customer by id - http://localhost:5000/api/tests/:id
+router.get('/:id', customerController.getById)
+
+// create customer - http://localhost:5000/api/customers
+router.post('/', customerController.create)
+
+// delete customer by id - http://localhost:5000/api/tests/:id
 router.delete('/:id', customerController.deleteById)
+
+// update customer by id - http://localhost:5000/api/tests/:id
+router.put('/:id', customerController.updateById)
 
 module.exports = router;
